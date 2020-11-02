a1a6c56f66784ff6a8b9f9fa4a1ce01c0af372ee
hmin6356 <73460133+hmin6356@users.noreply.github.com>
2020-11-02 17:24:45 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
+let 저항 = 0
 let strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
 basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    저항 = pins.analogReadPin(AnalogPin.P2)
+    if (저항 < 100) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (저항 < 200) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else if (저항 < 300) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (저항 < 400) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (저항 < 500) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (저항 < 600) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    } else if (저항 < 700) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    } else if (저항 < 800) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+    } else if (저항 < 900) {
+        strip.rotate(1)
+        strip.showRainbow(1, 360)
+    }
 })
